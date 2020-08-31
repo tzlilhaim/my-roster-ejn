8c2dd1fe5e99cb5c20d8a687be0b5e17d708571b
tzlilhaim <tzlilhaim480@gmail.com>
2020-08-31 12:48:46 +0300

    Added server specs & api manager class

---
+++ APIManager.js
@@ -0,0 +1,39 @@
+const urllib = require("urllib")
+const { error, Callbacks } = require("jquery")
+
+class APIManager {
+  constructor() {
+    this.teamToIDs = {
+      lakers: "1610612747",
+      warriors: "1610612744",
+      heat: "1610612748",
+      suns: "1610612756",
+    }
+  }
+  fetchPlayersInTeam(year, teamName) {
+    const am = this
+    urllib.request(
+      `http://data.nba.net/10s/prod/v1/${year}/players.json`,
+      function (err, players) {
+        if (err) {
+            console.log(err)
+          return err
+        } else {
+          players = JSON.parse(players)
+          const leagues = Object.keys(players.league)
+          const activePlayersInTeam = []
+          for (let leagueName of leagues) {
+            const playersInLeague = players.league[leagueName].filter(
+              (p) => p.teamId === am.teamToIDs[teamName] && p.isActive
+            )
+            activePlayersInTeam.push(...playersInLeague)
+          }
+          return activePlayersInTeam
+        }
+      }
+    )
+  }
+}
+
+const apiManager = new APIManager()
+module.exports = { manager: apiManager }
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const app = express()
+const api = require('./APIManager')
 
 app.use(express.static(path.join(__dirname, 'dist')))
 app.use('/scripts', express.static(__dirname + '/node_modules'))
@@ -9,3 +10,9 @@ const port = 3000
 app.listen(port, function () {
     console.log(`Running server on port ${port}`)
 })
+
+app.get("/teams/:teamName", function (request, response) {
+    const teamName = request.params.teamName.toLowerCase()
+    const year = "2018"
+    response.send(api.manager.fetchPlayersInTeam(year, teamName))
+  })
